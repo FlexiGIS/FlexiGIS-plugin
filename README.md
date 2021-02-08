e0fc76a544f659b8212ce55b0030a9bdd3f23656
chunai <uniachi.chinonso@gmail.com>
2021-02-08 12:07:27 +0100

    some comments on readme

--- README.md
+++ README.md
@@ -18,17 +18,17 @@ Fig. 1. A screen-shot of the experimental version of the FlexiGIS-GUI as a QGIS 
 terminal:~$ https://github.com/FlexiGIS/FlexiGIS-plugin.git
 ```
 
-- cd into the FlexiGIS-plugin directory and copy the  *flexgi_test* folder to yours QGIS3 python path.
+- cd into the FlexiGIS-plugin directory and copy the  *flexigis_plugin* folder to yours QGIS3 python path.
 
 ``` 
-terminal/FlexiGIS-plugin:~$ cp -r flexigi_test /home/user/.local/share/QGIS/QGIS3/profiles/default/python/plugins
+terminal/FlexiGIS-plugin:~$ cp -r flexigis_plugin ~/.local/share/QGIS/QGIS3/profiles/default/python/plugins
 ```
 
-After following the above steps, start your QGIS3 deskop environment. First install plugin reloader, this is used to reload any installed plugin in QGIS. To reload the flexgi_test plugin, ensure you can see it on the list of installed plugins in your QGIS3 (Plugin management section).
+After following the above steps, start your QGIS3 deskop environment. First install plugin reloader, this is used to reload any installed plugin in QGIS. To reload the flexgi_plugin, ensure you can see it on the list of installed plugins in your QGIS3 (Plugin management section).
 
 In your QGIS menu bar go to: plugins > Manage and install plugins > installed (to check installed plugin). To ensure that the newly created plugin is present in the QGIS plugin path.
 
-To reload plugin go to the plugin reloader icon, click on the drop down menu, select configuration, then select the plugin you want to reload. In our case we select the 'flexgi_test'. The final hit the reload icon of the plugin reloader. This is done any time the plugin backed or front end codes are edited.
+To reload plugin go to the plugin reloader icon, click on the drop down menu, select configuration, then select the plugin you want to reload. In our case we select the 'flexigis_plugin'. The final hit the reload icon of the plugin reloader. This is done any time the plugin backed or front end codes are edited. See further documentation [here](https://github.com/FlexiGIS/FlexiGIS-plugin/blob/master/flexigis_plugin/help/source/index.rst).
 
 ## Dependencies
 
--- flexigis_plugin/flexigis_utils.py
+++ flexigis_plugin/flexigis_utils.py
@@ -24,25 +24,27 @@ from qgis.PyQt.QtGui import QPolygonF, QColor
 
 
 def filter_pbf_with_poly(pbffile, polyfile, output_filename):
+    '''Filter OSM pbf file using a poly file.'''
     try:
         result = subprocess.run(["osmosis", "--read-pbf", "file="+str(pbffile),
-            "--tag-filter", "accept-ways", "building=*", "--used-node",
-                        "--bounding-polygon", "file=" +
-                            str(polyfile), "--buffer", "outPipe.0=building",
-                        "--read-pbf", "file=" +
-                            str(pbffile), "--tag-filter", "accept-ways", "highway=*", "--used-node",
-                        "--bounding-polygon", "file=" +
-                            str(polyfile), "--buffer", "outPipe.0=highway", "--read-pbf", "file="+str(pbffile),
-                        "--tag-filter", "accept-ways", "landuse=*", "--used-node", "--bounding-polygon", "file=" +
-                            str(polyfile),
-                        "--buffer", "outPipe.0=landuse_1", "--read-pbf", "file=" +
-                            str(pbffile), "--tag-filter", "accept-relations",
-                        "landuse=*", "--used-node", "--bounding-polygon", "file=" +
-                            str(polyfile), "--buffer", "outPipe.0=landuse_2",
-                        "--merge", "inPipe.0=landuse_1", "inPipe.1=landuse_2", "--buffer", "outPipe.0=landuse_all",
-                        "--merge", "inPipe.0=landuse_all", "inPipe.1=building", "--buffer", "outPipe.0=landuse_building",
-                        "--merge", "inPipe.0=landuse_building", "inPipe.1=highway", "--write-pbf", "file=" + str(output_filename)],
-                             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                                 "--tag-filter", "accept-ways", "building=*", "--used-node",
+                                 "--bounding-polygon", "file=" +
+                                 str(polyfile), "--buffer", "outPipe.0=building",
+                                 "--read-pbf", "file=" +
+                                 str(pbffile), "--tag-filter", "accept-ways", "highway=*", "--used-node",
+                                 "--bounding-polygon", "file=" +
+                                 str(polyfile), "--buffer", "outPipe.0=highway", "--read-pbf", "file="+str(
+                                     pbffile),
+                                 "--tag-filter", "accept-ways", "landuse=*", "--used-node", "--bounding-polygon", "file=" +
+                                 str(polyfile),
+                                 "--buffer", "outPipe.0=landuse_1", "--read-pbf", "file=" +
+                                 str(pbffile), "--tag-filter", "accept-relations",
+                                 "landuse=*", "--used-node", "--bounding-polygon", "file=" +
+                                 str(polyfile), "--buffer", "outPipe.0=landuse_2",
+                                 "--merge", "inPipe.0=landuse_1", "inPipe.1=landuse_2", "--buffer", "outPipe.0=landuse_all",
+                                 "--merge", "inPipe.0=landuse_all", "inPipe.1=building", "--buffer", "outPipe.0=landuse_building",
+                                 "--merge", "inPipe.0=landuse_building", "inPipe.1=highway", "--write-pbf", "file=" + str(output_filename)],
+                                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 # logging.info(result)
     except RuntimeError:
         pass
@@ -50,22 +52,25 @@ def filter_pbf_with_poly(pbffile, polyfile, output_filename):
 
 
 def osm_convert(in_file, out_file):
+    '''Convert osm.pbf file to .o5m file'''
     result = subprocess.run(["osmconvert", str(in_file), "-o=" + str(out_file) + ".o5m"],
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 # logging.info(result)
 
 
 def osm_filter(input_o5m, osm_tag, output_osm):
+    '''Convert .o5m file to .osm file'''
     result = subprocess.run(["osmfilter", str(input_o5m)+".o5m",
                              "--keep=" + str(osm_tag), "-o=" + str(output_osm) + ".osm"],
-                                  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     # logging.info(result)
 
 
 def osm_shapefile(output_osm):
+    ''' Convert osm to ghapefile'''
     result = subprocess.run(["ogr2ogr", "-skipfailures", "-f",
                              "ESRI Shapefile", str(output_osm), str(output_osm) + ".osm"],
-                                  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     # logging.info(result)
 
 
@@ -90,150 +95,158 @@ def compute_area(dataset, width):
 
 
 def filter_lines(shp_file, out_dir):
-	layer = QgsVectorLayer(shp_file, "", 'ogr')
-	highway_lines = {'living_street', 'motorway', 'pedestrian', 'primary', 'secondary', 'service', 'tertiary', 'trunk',
-					'motorway_link', 'primary_link', 'secondary_link',
-					'tertiary_link', 'trunk_link'}
-	width = [7.5, 15.50, 7.5, 10.5, 9.5, 7.5, 9.5, 9.5, 6.5, 6.5, 6.5, 6.5, 6.5]
+    ''''Filter Urban highway features'''
+    layer = QgsVectorLayer(shp_file, "", 'ogr')
+    highway_lines = {'living_street', 'motorway', 'pedestrian', 'primary', 'secondary', 'service', 'tertiary', 'trunk',
+                     'motorway_link', 'primary_link', 'secondary_link',
+                     'tertiary_link', 'trunk_link'}
+    width = [7.5, 15.50, 7.5, 10.5, 9.5, 7.5,
+             9.5, 9.5, 6.5, 6.5, 6.5, 6.5, 6.5]
 
-	highway_width = dict(zip(highway_lines, width))
+    highway_width = dict(zip(highway_lines, width))
 
-	osm_id = [
-		feature["osm_id"]
-		for feature in layer.getFeatures()
-		if feature["highway"] in highway_lines
-	]
+    osm_id = [
+        feature["osm_id"]
+        for feature in layer.getFeatures()
+        if feature["highway"] in highway_lines
+    ]
 
-	highway = [
-		feature["highway"]
-		for feature in layer.getFeatures()
-		if feature["highway"] in highway_lines
-	]
+    highway = [
+        feature["highway"]
+        for feature in layer.getFeatures()
+        if feature["highway"] in highway_lines
+    ]
 
-	crsSrc = QgsCoordinateReferenceSystem(4326)
-	crsDrc = QgsCoordinateReferenceSystem(3857)
-	tr = QgsCoordinateTransform(crsSrc, crsDrc, QgsProject.instance())
-	geom2 = []
-	length = []
+    crsSrc = QgsCoordinateReferenceSystem(4326)
+    crsDrc = QgsCoordinateReferenceSystem(3857)
+     tr = QgsCoordinateTransform(crsSrc, crsDrc, QgsProject.instance())
+      geom2 = []
+       length = []
 
-	for feature in layer.getFeatures():
-		if feature['highway'] in highway_lines:
-			geom = feature.geometry()
-			geom.transform(tr)
-			your_string = geom.asWkt()
-			length_ = round(geom.length(), 2)
-			geom2.append(your_string)
-			length.append(length_)
+        for feature in layer.getFeatures():
+            if feature['highway'] in highway_lines:
+                geom = feature.geometry()
+                geom.transform(tr)
+                your_string = geom.asWkt()
+                length_ = round(geom.length(), 2)
+                geom2.append(your_string)
+                length.append(length_)
 
-	df_line = pd.DataFrame(
-	    {
-		'osm_id': osm_id,
-		'highway': highway,
-		'length': length,
-		'geometry': geom2
-	    }
-	)
+        df_line = pd.DataFrame(
+            {
+                'osm_id': osm_id,
+                'highway': highway,
+                'length': length,
+                'geometry': geom2
+            }
+        )
 
-	df_line = compute_area(df_line, highway_width)
-	df_line = df_line.reset_index()
-	df_line = df_line[[
+        df_line = compute_area(df_line, highway_width)
+        df_line = df_line.reset_index()
+        df_line = df_line[[
             "highway", "osm_id", "length", "area", "geometry"]]
-	df_line.to_csv(os.path.join(out_dir, "highway_lines.csv"))
+        df_line.to_csv(os.path.join(out_dir, "highway_lines.csv"))
 
-#*************** Highway square ***********************
+# *************** Highway square ***********************
 
 
 def filter_squares(shp_file, out_dir):
-	layer = QgsVectorLayer(shp_file, "", 'ogr')
-	square_feature = {'crossing', 'footway', 'living_street',
-                   'pedestrian', 'platform', 'residential',
-                   'service', 'traffic_island'
-                   }
-	osm_id = [
-            feature["osm_id"]
-            for feature in layer.getFeatures()
-        ]
+    ''''Filter Urban highway features'''
+    layer = QgsVectorLayer(shp_file, "", 'ogr')
+    square_feature = {'crossing', 'footway', 'living_street',
+                      'pedestrian', 'platform', 'residential',
+                      'service', 'traffic_island'
+                      }
+    osm_id = [
+        feature["osm_id"]
+        for feature in layer.getFeatures()
+    ]
 
-	osm_way_id = [
-		feature["osm_way_id"]
-		for feature in layer.getFeatures()
-	]
+    osm_way_id = [
+        feature["osm_way_id"]
+        for feature in layer.getFeatures()
+    ]
 
-	# get available line features from tag
-	other_tags = [
-		feature["other_tags"]
-		for feature in layer.getFeatures()
-	]
+    # get available line features from tag
+    other_tags = [
+        feature["other_tags"]
+        for feature in layer.getFeatures()
+    ]
 
-	crsSrc = QgsCoordinateReferenceSystem(4326)
-	crsDrc = QgsCoordinateReferenceSystem(3857)
-	tr = QgsCoordinateTransform(crsSrc, crsDrc, QgsProject.instance())
-	geom2 = []
-	Area = []
+    crsSrc = QgsCoordinateReferenceSystem(4326)
+    crsDrc = QgsCoordinateReferenceSystem(3857)
+    tr = QgsCoordinateTransform(crsSrc, crsDrc, QgsProject.instance())
+     geom2 = []
+      Area = []
 
-	for feature in layer.getFeatures():
-		geom = feature.geometry()
-		geom.transform(tr)
-		your_string = geom.asWkt()
-		area_ = round(geom.area(), 2)
-		geom2.append(your_string)
-		Area.append(area_)
+       for feature in layer.getFeatures():
+            geom = feature.geometry()
+            geom.transform(tr)
+            your_string = geom.asWkt()
+            area_ = round(geom.area(), 2)
+            geom2.append(your_string)
+            Area.append(area_)
 
-	df_square = pd.DataFrame(
-	    {
-		'osm_id': osm_id,
-		'osm_way_id': osm_way_id,
-		'other_tags': other_tags,
-		'area': Area,
-		'geometry': geom2
-	    }
-	)
+        df_square = pd.DataFrame(
+            {
+                'osm_id': osm_id,
+                'osm_way_id': osm_way_id,
+                'other_tags': other_tags,
+                'area': Area,
+                'geometry': geom2
+            }
+        )
 
-	# filter other_tags attribute to extract highway square features
-	highway_shapes = df_square[df_square["other_tags"] !=NULL]
+        # filter other_tags attribute to extract highway square features
+        highway_shapes = df_square[df_square["other_tags"] != NULL]
     # create a clean highway tag column
-	tag_val = []
-	tag = "highway"
-	for i in highway_shapes.other_tags.values:
-		if tag in i:
-			tag_true = 1
-			tag_val.append(tag_true)
-		else:
-			tag_false = 0
-			tag_val.append(tag_false)
-	highway_shapes["bool"] = tag_val
-	highway_shapes = highway_shapes[highway_shapes["bool"] == 1]
-	filter_tag = []
-	for n in highway_shapes["other_tags"].values:
-		ll = n.split(",")
-		for lll in ll:
-			if lll.startswith('"highway"'):
-				filter_tag.append(lll)
-	feature_list = []
-	for i in filter_tag:
-		m = i.split('"')
-		feature_list.append(m[3])
-	highway_shapes["highway"] = feature_list
-	highway_shapes_df = highway_shapes.loc[highway_shapes["highway"].isin(
-	    square_feature)]
-	highway_shapes_df = highway_shapes_df.sort_values("highway")
-	highway_shapes_df = highway_shapes_df.reset_index()
-	highway_shapes_df = highway_shapes_df[[
-	    "osm_id", "osm_way_id", "highway",  "area", "geometry"]]
+        tag_val = []
+        tag = "highway"
+        for i in highway_shapes.other_tags.values:
+            if tag in i:
+                tag_true = 1
+                tag_val.append(tag_true)
+            else:
+                tag_false = 0
+                tag_val.append(tag_false)
+        highway_shapes["bool"] = tag_val
+        highway_shapes = highway_shapes[highway_shapes["bool"] == 1]
+        filter_tag = []
+        for n in highway_shapes["other_tags"].values:
+            ll = n.split(",")
+            for lll in ll:
+                if lll.startswith('"highway"'):
+                    filter_tag.append(lll)
+        feature_list = []
+        for i in filter_tag:
+            m = i.split('"')
+            feature_list.append(m[3])
+        highway_shapes["highway"] = feature_list
+        highway_shapes_df = highway_shapes.loc[highway_shapes["highway"].isin(
+            square_feature)]
+        highway_shapes_df = highway_shapes_df.sort_values("highway")
+        highway_shapes_df = highway_shapes_df.reset_index()
+        highway_shapes_df = highway_shapes_df[[
+            "osm_id", "osm_way_id", "highway",  "area", "geometry"]]
 
-	highway_shapes_df.to_csv(os.path.join(out_dir, "highway_squares.csv"))
+        highway_shapes_df.to_csv(os.path.join(out_dir, "highway_squares.csv"))
 
 # convert csv to shapefile
+
+
 def shape_to_csv(dir_path, layer_name):
-	in_csvfile = os.path.join(dir_path, layer_name)
-	out_shpfile = os.path.join(dir_path, os.path.splitext(in_csvfile)[0])
-	result = subprocess.run(["ogr2ogr", "-oo", "GEOM_POSSIBLE_NAMES=geometry*", "-f", "ESRI Shapefile", str(out_shpfile)+".shp", str(in_csvfile)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    ''' convert csv file to shapefile'''
+    in_csvfile = os.path.join(dir_path, layer_name)
+    out_shpfile = os.path.join(dir_path, os.path.splitext(in_csvfile)[0])
+    result = subprocess.run(["ogr2ogr", "-oo", "GEOM_POSSIBLE_NAMES=geometry*", "-f", "ESRI Shapefile", str(
+        out_shpfile)+".shp", str(in_csvfile)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def csvLayerNames(layer_path):
-	layer_names = glob.glob(os.path.join(layer_path, "*.csv"))
-	xx = [os.path.basename(i) for i in layer_names]
-	return xx
+    '''get csv file paths'''
+    layer_names = glob.glob(os.path.join(layer_path, "*.csv"))
+    xx = [os.path.basename(i) for i in layer_names]
+    return xx
 
 
 # simulate street light electrcity demand
@@ -252,30 +265,32 @@ def streetLightDemnd(input_path, input_path2, output_path):
     # get planet OSM data for highway (line and polygon)
     osmLines = pd.read_csv(os.path.join(input_path2, 'highway_lines.csv'))
     osmSquares = pd.read_csv(os.path.join(input_path2, 'highway_squares.csv'))
-    osmData = pd.concat([osmLines[["highway", "area"]], osmSquares[["highway", "area"]]], ignore_index=True)
+    osmData = pd.concat([osmLines[["highway", "area"]],
+                         osmSquares[["highway", "area"]]], ignore_index=True)
     # for secenario 1 & 2
     osmArea = osmData["area"].sum()
     squaresArea = osmSquares["area"].sum()
     # scenario 3 => main roads only for all night
     mainRoads = ['living_street', 'motorway', 'pedestrian', 'primary',
-                    'secondary', 'service', 'tertiary', 'trunk']
+                 'secondary', 'service', 'tertiary', 'trunk']
     mainRoadsArea = osmLines[osmLines["highway"].
-                                        isin(mainRoads)]["area"].sum()
+                             isin(mainRoads)]["area"].sum()
     # scenario 4 => minus the selected main roads
     # operated using SL1 (on and off)
     secondaryRoadsArea = osmLines[~osmLines["highway"].
-                                            isin(mainRoads)]["area"].sum()
+                                  isin(mainRoads)]["area"].sum()
     mainRoadandsquareArea = mainRoadsArea + squaresArea
 
     _streetLoad_ = []
     fname = open(os.path.join(output_path,
-                                "streetlight_load.csv"), 'w')
+                              "streetlight_load.csv"), 'w')
     fname.write('TIME;Mode2;Mode1;Mode3\n')
     for i, row in standardLoad.iterrows():
         row = str(timestamp[i]) + ';' + \
             str(osmArea*SL1[i]*x_sl) + ';' +\
             str(osmArea*SL2[i]*x_sl) + ';' +\
-            str((mainRoadandsquareArea*SL2[i]*x_sl) + (secondaryRoadsArea*SL1[i]*x_sl)) + '\n'
+            str((mainRoadandsquareArea*SL2[i]*x_sl) +
+                (secondaryRoadsArea*SL1[i]*x_sl)) + '\n'
         _streetLoad_.append(row)
     fname.writelines(_streetLoad_)
     fname.close()
@@ -301,10 +316,12 @@ def optimizationCommodities(input_path, input_path2, output_path):
     feedin.to_csv(os.path.join(
         output_path, 'optimization-commodities.csv'))
 
-#********************export layer to map**********************
+# ********************export layer to map**********************
+# Inspired by code from https://gis.stackexchange.com/questions/175068/applying-categorized-symbol-to-each-feature-using-pyqgis
 
 
 def symbolize_layer(dir_path, layerfile_name):
+    '''create a categorized map layer of the urban features'''
     layer_path = os.path.join(dir_path, layerfile_name)
     file_name = os.path.splitext(layer_path)
     layer_name = os.path.basename(file_name[0])
@@ -326,10 +343,10 @@ def symbolize_layer(dir_path, layerfile_name):
         print('Layer field incomplete')
 
     unique_values = layer.uniqueValues(unique_layer)
-    #fill categories
+    # fill categories
     categories = []
     for unique_value in unique_values:
-        #initialize the default symbol for this geometry
+        # initialize the default symbol for this geometry
         symbol = QgsSymbol.defaultSymbol(layer.geometryType())
 
         # configure symbol layer
@@ -356,7 +373,7 @@ def symbolize_layer(dir_path, layerfile_name):
     else:
         print('Layer field incomplete')
 
-    #assign the created renderer to the layer
+    # assign the created renderer to the layer
     if renderer is not None:
         layer.setRenderer(renderer)
 
@@ -423,10 +440,9 @@ def symbolize_layer(dir_path, layerfile_name):
 # 	scalebar.attemptResize(QgsLayoutSize(
 # 		134.753, 12.500, QgsUnitTypes.LayoutMillimeters))
 
-	# export layout to png or pdf
-	#layout = manager.layoutByName(layoutName)
-	#exporter = QgsLayoutExporter(layout)
-	#map_name = os.path.join(out_path,'map.png')
-	#exporter.exportToImage(map_name, QgsLayoutExporter.ImageExportSettings())
-	#exporter.exportToPdf(map_name, QgsLayoutExporter.PdfExportSettings())
-
+    # export layout to png or pdf
+    #layout = manager.layoutByName(layoutName)
+    #exporter = QgsLayoutExporter(layout)
+    #map_name = os.path.join(out_path,'map.png')
+    #exporter.exportToImage(map_name, QgsLayoutExporter.ImageExportSettings())
+    #exporter.exportToPdf(map_name, QgsLayoutExporter.PdfExportSettings())
